15fecc280971076e7b969b2bf532376f71850506
Do Dang Minh Luan <dodangminhluan@gmail.com>
2026-07-10 11:28:14 +0700

    chore: stop tracking generated artifacts

--- apps/worker-node/dist/main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = require("./app");
-const config_service_1 = require("./modules/config/config.service");
-const config = config_service_1.configService.getWorkerConfig(process.env);
-const app = (0, app_1.createApp)();
-app.listen(config.port, () => {
-    console.log(`[Lutest Worker] Ready on port ${config.port} | Mode: ${config.env} | Timeout: ${config.workerTimeoutMs}ms`);
-});
